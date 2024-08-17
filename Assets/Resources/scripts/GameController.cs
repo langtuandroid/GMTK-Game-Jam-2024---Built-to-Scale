519b65e8f94385417e533362d8b0362c0411ded1
Shane Gadsby <Shane.Gadsby@unisq.edu.au>
2024-08-17 21:28:51 +1000

    switch to free fly camera

--- Assets/Resources/scripts/GameController.cs
+++ Assets/Resources/scripts/GameController.cs
@@ -1,6 +1,7 @@
 using Cinemachine;
 using UnityEngine;
 using Dreamteck.Splines;
+using UnityEngine.InputSystem.Processors;
 public class GameController : MonoBehaviour {
 	public string state;
 	private string lastState;
@@ -16,6 +17,7 @@ public class GameController : MonoBehaviour {
 
 
 	[Header("Player stuff")]
+	public Transform player;
 	public SplineFollower playerFollower;
 	public SplinePositioner playerPositioner;
 	public CinemachineVirtualCamera virtualCamera;
@@ -83,11 +85,11 @@ public class GameController : MonoBehaviour {
 				var movement = Input.GetAxis("Vertical");
 
 				if (movement != 0) {
-					
-					
-					
-					
 
+
+
+
+					/*
 					//Get the current position on the splice as a percentage
 					var playerPercent = playerFollower.result.percent;
 
@@ -108,8 +110,47 @@ public class GameController : MonoBehaviour {
 
 					//Set the player movement direction based on the camera forwards direction, based on the dot product evaluating to: x > 0 = forwards, x =< 0 == backwards
 					playerFollower.direction = Vector3.Dot(virtualCameraForwards, playerDirection) > 0 ? Spline.Direction.Forward : Spline.Direction.Backward;
+					*/
 
 
+
+					/*
+					// Get the current position on the spline as a percentage
+					var playerPercent = playerFollower.result.percent;
+
+					// Evaluate the next direction as a normalized Vector3
+					var playerDirection = (playerFollower.spline.EvaluatePosition(playerPercent + 0.01f) - playerFollower.spline.EvaluatePosition(playerPercent)).normalized;
+
+					// Get the virtual camera's forward vector
+					var virtualCameraForwards = virtualCamera.transform.forward;
+
+					// Clear the Y component of the virtual camera's forward vector
+					virtualCameraForwards.y = 0;
+					virtualCameraForwards.Normalize(); // Normalize after clearing Y
+
+					// Get the angle between the virtual camera's forward vector and the player's direction
+					var angle = Quaternion.Angle(Quaternion.LookRotation(virtualCameraForwards), Quaternion.LookRotation(playerDirection));
+
+					// Set the player movement direction based on the angle
+					playerFollower.direction = angle <= 90 ? Spline.Direction.Forward : Spline.Direction.Backward;
+
+					// If the input movement is less than 0, invert the movement direction
+					if (movement < 0) {
+						// Invert direction
+						playerFollower.direction = playerFollower.direction == Spline.Direction.Forward
+							? Spline.Direction.Backward
+							: Spline.Direction.Forward;
+					}
+
+					// Calculate the movement speed based on how aligned the camera is with the spline direction
+					float alignmentFactor = 1 - (angle / 180f); // Closer to 0 means less aligned, closer to 1 means more aligned
+
+					// Move the player along the spline in the direction determined above
+					playerFollower.Move(Mathf.Abs(movement) * alignmentFactor * (Input.GetButton("Boost") ? 0.5f : 0.1f));
+					*/
+
+
+					/*
 					//If the input movement is less than 0, invert the movement direction
 					if (movement < 0) {
 
@@ -131,10 +172,10 @@ public class GameController : MonoBehaviour {
 
 					//Move the player along the spline in the direction determined above
 					playerFollower.Move(Mathf.Abs((movement) * (Input.GetButton("Boost") ? 0.5f : 0.1f)) * Mathf.Abs(playerDirectionDot));
+					*/
 
 
-					//playerFollower.direction = movement > 0 ? Spline.Direction.Forward : Spline.Direction.Backward;
-					//playerFollower.Move(Mathf.Abs(movement * (Input.GetButton("Boost") ? 0.5f : 0.1f)));
+
 				}
 
 				break;
